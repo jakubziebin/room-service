fc6a238977c9c9a7f0b9db6999c4c5f4afe4ddd4
Jakub Ziębiński <ziebinskijakub@gmail.com>
2023-08-27 19:49:16 +0200

    Add my first dynamically displayed parameters

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
+from __future__ import annotations
+
+from typing import Final, Any
+
 from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
+PARAMETERS: Final[list[dict[str, Any]]] = [
+    {"id": 1, "title": "humidity", "unit": "%"},
+    {"id": 2, "title": "particles", "unit": "uq"},
+    {"id": 3, "title": "temperature", "unit": "°C"},
+]
+
 
 @app.route("/")
-def hello_world():
-    return render_template("home.html")
+def hello_world() -> Any:
+    return render_template("home.html", parameters=PARAMETERS)
 
 
 if __name__ == "__main__":
